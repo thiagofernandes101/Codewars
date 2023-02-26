84cfa3ab3ce950baba1bc13cf0887f88a11dc256
thiagofernandes101 <thiagofernande101@outlook.com>
2023-02-25 23:55:12 -0300

    Codewars: String ends with?

---
+++ StringEndsWith/StringEndsWith/Kata.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace StringEndsWith
+{
+    internal class Kata
+    {
+        public static bool Solution(string str, string ending)
+        {
+            return str.EndsWith(ending);
+        }
+    }
+}
---
+++ StringEndsWith/StringEndsWith/Program.cs
@@ -0,0 +1,24 @@
+﻿namespace StringEndsWith
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine(Kata.Solution("samurai", "ai"));
+            Console.WriteLine(Kata.Solution("sumo", "omo"));
+            Console.WriteLine(Kata.Solution("ninja", "ja"));
+            Console.WriteLine(Kata.Solution("sensei", "i"));
+            Console.WriteLine(Kata.Solution("samurai", "ra"));
+            Console.WriteLine(Kata.Solution("abc", "abcd"));
+            Console.WriteLine(Kata.Solution("abc", "abc"));
+            Console.WriteLine(Kata.Solution("abcabc", "bc"));
+            Console.WriteLine(Kata.Solution("ails", "fails"));
+            Console.WriteLine(Kata.Solution("fails", "ails"));
+            Console.WriteLine(Kata.Solution("this", "fails"));
+            Console.WriteLine(Kata.Solution("abc", ""));
+            Console.WriteLine(Kata.Solution(":-)", ":-("));
+            Console.WriteLine(Kata.Solution("!@#$%^&*() :-)", ":-)"));
+            Console.WriteLine(Kata.Solution("abc\n", "abc"));
+        }
+    }
+}
